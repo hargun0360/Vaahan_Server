471f86d03420a44f184acaf6b7f7455acdcc3646
hargun0360 <hargunsingh0720@gmail.com>
2024-05-17 18:29:52 +0530

    Add Cors

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import routes from "./routes.js";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
@@ -9,7 +10,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use("/api", routes);
-
+app.use(cors());
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });

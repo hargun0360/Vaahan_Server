e60bbb6972121c2d712040345530302f195f915b
hargun0360 <hargunsingh0720@gmail.com>
2024-05-20 00:11:09 +0530

    Deployement

--- src/db.ts
+++ src/db.ts
@@ -6,7 +6,7 @@ dotenv.config();
 export const db = knex({
   client: "pg",
   connection: {
-    host: process.env.POSTGRES_URL,
+    host: process.env.POSTGRES_HOST,
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DATABASE,
